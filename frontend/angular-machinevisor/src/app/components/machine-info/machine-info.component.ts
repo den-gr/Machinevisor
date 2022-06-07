93bc590055d3441efe31e7d46cf1d0bf0de05cf6
Elisa Albertini <elisa.albertini3@studio.unibo.it>
2022-06-07 12:16:22 +0200

    - Added colored values in machine-info component

--- frontend/angular-machinevisor/src/app/components/machine-info/machine-info.component.ts
+++ frontend/angular-machinevisor/src/app/components/machine-info/machine-info.component.ts
@@ -13,6 +13,7 @@ export class MachineInfoComponent implements OnInit {
   status = 'Error';
   photo = '../../../assets/img/ciambella.jpeg';
   infoMix = Array()
+  valuesMix = Array()
 
   clickCharts(){
     console.log("Go to Charts");
@@ -24,13 +25,22 @@ export class MachineInfoComponent implements OnInit {
 
   ngOnInit(): void {
     let info = Array({"Weight":"100kg", "Year":"2010", "Key3":"Val3", "Key4":"Val4", "Key5":"Val5"})
+    let values = Array({"Value1":["123", true], "Value2":["456", false], "Value3":["789", true], "Value4":["1011", true], "Value5":["1213", true]})
 
     let infoKey = Object.keys(info[0])
     let infoVal = Object.values(info[0])
+    let valuesKey = Object.keys(values[0])
+    let valuesVal = Object.values(values[0])
 
     for(let i=0; i<infoKey.length; i++){
       this.infoMix.push({key:infoKey[i], value:infoVal[i]})
     }
+
+    for(let i=0; i<valuesKey.length; i++){
+      this.valuesMix.push({key:valuesKey[i], value:valuesVal[i][0], error:valuesVal[i][1]})
+    }
+
+    console.log(this.valuesMix)
   }
 
 }
