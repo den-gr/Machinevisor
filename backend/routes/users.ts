d694f80248eac3ade9536406ab152fc725977e11
Denys <denys.grushchak@studio.unibo.it>
2022-06-08 20:12:40 +0200

    minor fix

--- backend/routes/users.ts
+++ backend/routes/users.ts
@@ -4,13 +4,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/:userId', function(req:Request, res:Response) {
-  if(req.params.userId){
     res.send('respond with a resource ' + req.params.userId);
-
-  }else{
-    res.send("no value");
-  }
-
 });
 
 module.exports = router;
