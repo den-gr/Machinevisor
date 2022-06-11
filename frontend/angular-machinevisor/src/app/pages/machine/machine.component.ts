d78382b11d4a6961968be21f20565ce5d3011970
Elisa Albertini <elisa.albertini3@studio.unibo.it>
2022-06-11 17:56:21 +0200

    - Creation proxy on frontend - Add call to API in pages\machine

--- frontend/angular-machinevisor/src/app/pages/machine/machine.component.ts
+++ frontend/angular-machinevisor/src/app/pages/machine/machine.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-machine',
@@ -7,11 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MachineComponent implements OnInit {
 
-  constructor() { }
+  constructor( private http: HttpClient) { }
 
   machineID = 'machine-1'
 
+  getMachineData(){
+    const url = `${environment.apiUrl}/machines/1`;
+    this.http.get(url).subscribe(responce => {
+      console.log("Responce :) --> " + JSON.stringify(responce));
+    });
+  }
+
   ngOnInit(): void {
+    this.getMachineData();
   }
 
 }
