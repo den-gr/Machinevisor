7c158593da30e5fa53ce2ed6cbd10420a82793ac
Denys <denys.grushchak@studio.unibo.it>
2022-06-30 15:56:20 +0200

    change kWh in kWatt

--- backend/src/machine_simulation/machine_manager.ts
+++ backend/src/machine_simulation/machine_manager.ts
@@ -1,8 +1,10 @@
 import { MachineSimulation } from "./machine";
 import { Modality } from "../utils/utils";
 
+const HOST = "https://localhost:8080";
+
 let id = 1;
 let machineModalitiesType_1: Modality[] = [Modality.SLEEP_MODE, Modality.PRODUCTION_MODE, Modality.ENERGY_ECONOMY_PRODUCTION_MODE]
 
-const mach: MachineSimulation = new MachineSimulation("https://localhost:8080", id, machineModalitiesType_1);
+const mach: MachineSimulation = new MachineSimulation(HOST, id, machineModalitiesType_1);
 // const mach2: MachineSimulation = new MachineSimulation("https://localhost:8080", 2);
